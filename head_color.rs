3645101fc4b2fa72cf05ad668907b09190be7d5a
Kirill <Akavall@gmail.com>
2015-10-28 22:32:57 -0500

    works on two simple examples

--- head_color.rs
+++ head_color.rs
@@ -8,34 +8,72 @@ fn main() {
     let file_name = &env::args().nth(1).unwrap();
     let col_number_str = &env::args().nth(2).unwrap();
     let col_number: usize = col_number_str.parse::<usize>().unwrap();
-    let delim = &env::args().nth(3).unwrap();
+
+    // delim needs to char type
+    let delim_raw = &env::args().nth(3).unwrap();
+    let temp: Vec<char> = delim_raw.chars().collect();
+    let delim = temp[0];
+
     let n_lines_str = &env::args().nth(4).unwrap();
     let n_lines: usize = n_lines_str.parse::<usize>().unwrap();
 
     let f = File::open(file_name).unwrap();
     let reader = BufReader::new(f);
 
-    let green = "\x1b[0;32m";
-    let nc = "\x1b[0m";
-
     let mut row_number = 0;
 
     for line in reader.lines() {
-        if row_number > n_lines {
+        if row_number >= n_lines {
             break;
         }
         let line = line.unwrap();
-        let my_vec: Vec<&str> = line.split(",").collect();
-        let colored_string = "".to_string() + green + my_vec[col_number] + nc;
-
-        for i in 0..col_number {
-            print!("{}{}", my_vec[i], delim);
-        }
-        print!("{}", colored_string);
-        for i in ((col_number+1))..(my_vec.len()) {
-            print!("{}{}", delim, my_vec[i]);
-        }
-        println!("");
+        color_col(line.to_string(), col_number, delim);
         row_number += 1;
     }
+}
+
+fn color_col(my_string: String, col: usize, delim: char) {
+
+    let mut col_tracker: usize = 0;
+    let mut temp: String = "".to_string();
+    let green: &str = "\x1b[0;32m";
+    let no_color: &str = "\x1b[0m";
+
+    let mut quote = false;
+
+    for c in my_string.chars() {
+        if c == '"' {
+            if quote == false {
+                quote = true
+            } else {
+                quote = false
+            }
+        }
+        
+        if c == delim && quote == false {
+            if col_tracker == col {
+                let mut colored: String = "".to_string();
+                colored.push_str(green);
+                colored.push_str(&temp);
+                colored.push_str(no_color);
+                print!("{}", colored);
+                } else {
+                print!("{}", temp);
+            }
+            temp = "".to_string();
+            col_tracker += 1;
+
+        }
+        temp.push(c);
+    }
+    
+    if col_tracker == col {
+        let mut colored: String = "".to_string();
+        colored.push_str(green);
+        colored.push_str(&temp);
+        colored.push_str(no_color);
+        println!("{}", colored);
+        } else {
+            println!("{}", temp);
+        }
 }
