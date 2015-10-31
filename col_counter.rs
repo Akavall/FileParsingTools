49f8f37bba1474ac22c2a6e8c4f6770c3d1e306b
Kirill <Akavall@gmail.com>
2015-10-31 09:13:01 -0500

    seems to be working

--- col_counter.rs
+++ col_counter.rs
@@ -6,7 +6,6 @@ use std::collections::HashMap;
 
 fn main() {
 
-    let mut col_string: &str = "";
     let file_name = &env::args().nth(1).unwrap();
     let col_number_str = &env::args().nth(2).unwrap();
     let col_number: usize = col_number_str.parse::<usize>().unwrap();
@@ -21,26 +20,42 @@ fn main() {
     let f = File::open(file_name).unwrap();
     let reader = BufReader::new(f);
 
-    let mut my_counter: HashMap<&str, usize> = HashMap::new();
+    let mut my_counter: HashMap<String, usize> = HashMap::new();
 
     for line in reader.lines() {
 
         let line = line.unwrap();
-        let col_String = color_col(line.to_string(), col_number, delim);
-        col_string = &*col_String;
-
-        if my_counter.contains_key(col_string) {
-            *my_counter.get_mut(col_string).unwrap() += 1;
+        let col_string = color_col(line.to_string(), col_number, delim);
+        
+        if my_counter.contains_key(&col_string) {
+            *my_counter.get_mut(&col_string).unwrap() += 1;
         } else {
             my_counter.insert(col_string, 1);
         }
     }
 
-    let mut my_counter_items: Vec<(&str, usize)> = my_counter.clone().into_iter().collect();
+    let mut my_counter_items: Vec<(String, usize)> = my_counter.clone().into_iter().collect();
     my_counter_items.sort_by(|a, b| b.1.cmp(&a.1));
 
-    for i in 0..n_lines {
-        println!("{:?}", my_counter_items[i]);
+    let n_lines_to_print;
+    if my_counter_items.len() < n_lines {
+        n_lines_to_print = my_counter_items.len();
+    } else {
+        n_lines_to_print = n_lines;
+    }
+
+    let green: &str = "\x1b[0;33m";
+    let no_color: &str = "\x1b[0m"; 
+
+    let mut header: String = "".to_string();
+    header.push_str(green);
+    header.push_str("Rank: Value: Count");
+    header.push_str(no_color);
+
+    println!("{}", header);
+
+    for ind in 0..(n_lines_to_print) {
+        println!("{}: {}: {}", ind+1, my_counter_items[ind].0, my_counter_items[ind].1);
     }
     
 }
@@ -52,14 +67,15 @@ fn main() {
 fn color_col(my_string: String, col: usize, delim: char) -> String {
 
     let mut col_tracker: usize = 0;
-    let mut temp: String = "".to_string();
-
+    
     let mut quote = false;
     let mut col_string = "".to_string();
 
     for c in my_string.chars() {
-        if col == col_tracker && c != delim {
-            col_string.push(c)
+        if col == col_tracker {
+            if c != delim || quote == true {
+                col_string.push(c)
+            }
         }
         
         if c == '"' {
